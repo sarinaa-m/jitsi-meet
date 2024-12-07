a055644e7f1e7a9e99681434d4a4fca242a9e414
sarin.moayedi <sarin.moayedi@gmail.com>
2024-12-07 17:37:08 +0330

    feat(firebase): integrate Firebase configuration and update TypeScript settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,13 @@ module.exports = {
     'extends': [
         '@jitsi/eslint-config'
     ],
-    'ignorePatterns': [ '*.d.ts' ]
+    'ignorePatterns': [ '*.d.ts' ],
+    'parserOptions': {
+        'project': [
+            './tsconfig.web.json',
+            './tsconfig.native.json',
+            './tsconfig.firebase.json' // Add this line
+        ]
+    }
+
 };
